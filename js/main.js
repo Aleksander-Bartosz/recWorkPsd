9727c64a76a1a94c4b6377a8dabbb7f595d2a5db
Aleksander-Bartosz <alex312@op.pl>
2017-11-24 17:31:15 +0100

    'hamburger'

--- js/main.js
+++ js/main.js
@@ -2,4 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     window.onresize = function(event) {
         console.log($(this).innerWidth());
     };
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+        if (!$(this).hasClass('open')) {
+            let rwd = $('.rwdmenu');
+            rwd.animate({
+               top:'-120px', 
+            },1000)
+        }
+		$(this).toggleClass('open');
+	});
+    $('.cross').click(function () {
+        let rwd = $('.rwdmenu');
+            rwd.animate({
+               top:'-325px', 
+            },1000)
+        $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').toggleClass('open');
+    })
 });
